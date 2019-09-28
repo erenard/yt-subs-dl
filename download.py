d527d6c29274cdfd605641df8fbecf141a2ae145
Éric Renard <timmy469@gmail.com>
2019-09-28 18:22:30 +0200

    Call youtube-dl from python

---
+++ download.py
@@ -0,0 +1,25 @@
+"""Script to download my youtube stuff"""
+import sys
+import youtube_dl
+
+def main():
+	"""Main entry point"""
+	downloadUrl('https://www.youtube.com/user/Jayztwocents/videos')
+	pass
+
+def downloadUrl(url):
+	ydl_opts = {
+		"download_archive": "archive.txt",
+		"restrictfilenames": True,
+		"format": "best",
+		"playlistend": 15
+	}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		try:
+			result = ydl.download([url])
+		except Exception as inst:
+			print(inst)   
+	pass
+
+if __name__ == '__main__':
+	sys.exit(main())
