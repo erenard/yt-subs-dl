b089093bbb1e2e9317e635f18098d20f47291d09
Éric Renard <timmy469@gmail.com>
2021-10-31 09:44:47 +0100

    awk

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-youtube-dl --config-location ./config.audio.txt
-youtube-dl --config-location ./config.best.txt
+youtube-dl --config-location ./config.audio.txt | awk "! ( /Downloading video/ || /Downloading webpage/ || /upload date is not in range/ || /has already been recorded in archive/)"
+youtube-dl --config-location ./config.best.txt | awk "! ( /Downloading video/ || /Downloading webpage/ || /upload date is not in range/ || /has already been recorded in archive/)"
