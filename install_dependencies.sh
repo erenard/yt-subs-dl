7f26a10ed2cee9400755b92c6329d606587e0b11
erenard <timmy469@gmail.com>
2023-06-03 23:22:56 +0200

    transfert

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,4 @@
 termux-setup-storage
 pkg install -y python aria2
-pip install --upgrade youtube-dl
+python3 -m pip install --force-reinstall https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz
+#pip install --upgrade youtube-dl
